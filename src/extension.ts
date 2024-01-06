05532d6bd037473b8c9d0d1bc6af56c7b039782d
contigen <69436315+contigen@users.noreply.github.com>
2024-01-06 16:06:08 +0100

    Add ext's webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,70 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
+function isSVG(text: string): boolean {
+  return /<svg.*<\/svg>/i.test(text);
+}
+
+function showSVGPanel(selectedText: string) {
+  if (isSVG(selectedText)) {
+    const panel = vscode.window.createWebviewPanel(
+      "svgViewer",
+      "SVG Viewer",
+      vscode.ViewColumn.Beside,
+      {}
+    );
+    panel.webview.html = selectedText;
+  } else {
+    vscode.window.showInformationMessage(
+      "Selected text is not an SVG element."
+    );
+  }
+}
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(`Congratulations, your extension "svg-onselect" is now active!`);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "svg-onselect" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "svg-onselect.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      const panel = vscode.window.createWebviewPanel(
+        "svgViewer",
+        "SVG Viewer",
+        vscode.ViewColumn.One,
+        {}
+      );
+      panel.webview.html = getWebviewContent();
+      // const editor = vscode.window.activeTextEditor;
+      // const selectedText = editor?.document.getText(editor.selection);
+      // showSVGPanel(selectedText ?? ``);
+      vscode.window.showInformationMessage(
+        `Hello World from svg-onselect, yay!`
+      );
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('svg-onselect.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from svg-onselect!');
-	});
+  context.subscriptions.push(disposable);
+}
 
-	context.subscriptions.push(disposable);
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
 }
 
 // This method is called when your extension is deactivated
